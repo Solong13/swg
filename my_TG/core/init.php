9778833a1a7c411ab9bf3ee7453ccd6f0bc47e02
Solong13 <limbo24gg@gmail.com>
2023-04-17 22:32:25 +0300

    v2

---
+++ my_TG/core/init.php
@@ -0,0 +1,11 @@
+<?php
+define("TG_TOKEN", "5628796296:AAGu5HwepHC2HCtn3GypxI0bCjMplhqAllw");
+define("TG_USER_ID", 951454643);// user
+
+define("API_KEY", "e99f35585cf6c29e559267670e848778");
+define("TRELLO_TOKEN", "fdd99f3912a1238e6af379efcdae87aa3da95789469a27cc58d2903c658133d3");
+
+//DB
+$db_Name = 'dev_candidate_5';
+$db_password = 'bGwRBLAyHR';
+$db_host = 'localhost';
